02fe3fa8b03e7a8398b786616869da75747baef3
Bobby D <BobDole@gmail.com>
2020-03-11 12:02:36 -0500

    Adam integrated his ui code with the group project

---
+++ Collisions/Assets/Scripts/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public int timeUntilDestroy = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Destroy the projectile at 2 seconds
+        Object.Destroy( gameObject , 2.0f );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gameObject.GetComponent<Rigidbody>( ).velocity = transform.forward * speed;
+
+        Vector3 startPos = transform.position;
+        Quaternion shotAngle = transform.rotation;
+
+        //gameObject.GetComponent<Rigidbody>( ).AddForce( transform.forward * speed , ForceMode.Impulse );
+
+    }
+}
